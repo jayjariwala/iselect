713e9a649f53579127a03c30c88938b868f06252
jayjariwala <jay.jariwala@hotmail.com>
2018-06-12 14:36:52 -0400

    Successfully manipulating XML file

---
+++ src/generateXML.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+      parseString = require('xml2js').parseString;
+      xml2js = require('xml2js');
+      
+module.exports = function generateXML(jsondata) {
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(jsondata);
+  
+  fs.writeFile('edited-test.xml', xml, function(err, data) {
+      if (err) console.log(err);
+      console.log("successfully written our update xml to file");
+  })
+};
---
+++ src/main.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipc = electron.ipcMain;
+const treeLoader = require('./threeloader.js');
+const generateXML =  require('./generateXML.js');
+let mainWindow;
+
+app.on('ready', _=> {
+  mainWindow = new BrowserWindow();
+  mainWindow.loadURL(`file://${__dirname}/navigation.html`);
+  ipc.on('load_tree_data', _=> {
+    treeLoader(json => {
+      mainWindow.webContents.send('jsondata', json);
+    });
+  })
+
+  ipc.on('generateXML', (event, jsondata) => {
+    generateXML(jsondata);
+  })
+
+  mainWindow.on('closed', _=>{
+    console.log("closed");
+    mainWindow = null;
+  })
+})
+
